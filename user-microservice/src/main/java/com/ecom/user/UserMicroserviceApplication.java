c116716ba74d8b01e282c97a15bff7490b609d6b
kartavya-p <kartavya.98p@gmail.com>
2026-05-14 22:05:40 +0530

    Config server and user-ms configurations added and seperated

--- user-microservice/src/main/java/com/ecom/user/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/com/ecom/user/UserMicroserviceApplication.java
@@ -3,12 +3,18 @@ package com.ecom.user;
 import java.util.TimeZone;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class UserMicroserviceApplication {
+	@Autowired
+	private Environment environment;
 	@Bean
 	ModelMapper modelMapper() {
 		return new ModelMapper();
@@ -17,6 +23,18 @@ public class UserMicroserviceApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(UserMicroserviceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(MongoTemplate mongoClient) {
+		return args -> {
+			String dbName = mongoClient.getDb().getName();
+			System.out.println("Connected DB : " + dbName);
+
+			System.out.println(environment.getProperty("spring.mongodb.database"));
+			System.out.println(environment.getProperty("spring.mongodb.uri"));
+
+		};
 	}
 
 }
